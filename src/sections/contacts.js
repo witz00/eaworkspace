637b111a3a2d43ea36c20621dde64dd5bf9be837
witz00 <e.akhtyamkin@gmail.com>
2026-07-07 02:49:36 +0400

    Delete src directory

--- src/sections/contacts.js
+++
@@ -1,8 +0,0 @@
-export function renderContacts(container) {
-  container.innerHTML = `
-    <div class="content-area">
-      <span class="section-label">Коммуникация</span>
-      <p class="greeting-main">Раздел в разработке.</p>
-    </div>
-  `
-}
